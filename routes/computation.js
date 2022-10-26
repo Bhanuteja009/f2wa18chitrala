ded13123249c4b7d4e6d54ea196d86fd07ee5c98
Bhanuteja009 <111711705+Bhanuteja009@users.noreply.github.com>
2022-10-26 16:00:04 -0500

    updated computation.js

--- routes/computation.js
+++ routes/computation.js
@@ -3,11 +3,26 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  var x=Math.random();
   
-  res.render('computation', { value: `math.log2() applied to ${x} is ${Math.log2(x)}`,
-  value1: `math.cosh() applied to ${x} is ${Math.cosh(x)}` ,
-  value2: `math.floor() applied to ${x} is ${Math.floor(x)}` });
+  
+  if (Object.keys(req.query).length === 0) {
+    var x=Math.random();
+    res.render('computation', { value: `math.log2() applied to ${x} is ${Math.log2(x)}`,
+    value1: `math.cosh() applied to ${x} is ${Math.cosh(x)}` ,
+    value2: `math.floor() applied to ${x} is ${Math.floor(x)}` });
+  }
+  else{
+
+    for (let a in req.query) {
+
+      console.log(a)
+
+      res.render('computation', { value: `math.log2() applied to ${req.query[a]} is ${Math.log2(req.query[a])}`,
+      value1: `math.cosh() applied to ${req.query[a]} is ${Math.cosh(req.query[a])}` ,
+      value2: `math.floor() applied to ${req.query[a]} is ${Math.floor(req.query[a])}` })
+
+    }
+  }
 
 
 });
